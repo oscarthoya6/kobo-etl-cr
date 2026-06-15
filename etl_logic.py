c155a63a9a3aadde9d0e3a650475d2ce22817363
oscar <oscarthoya91@gmail.com>
2026-06-15 18:57:57 +0300

    Refactor ETL logic: shorten columns in extract step and fix duplicates

--- etl_logic.py
+++ etl_logic.py
@@ -3,16 +3,33 @@ from google.cloud import bigquery
 from koboextractor import KoboExtractor
 
 def extract_data(api_key, end_point, form_id):
+    """
+    Extract data from KoboToolbox and return a raw dataframe
+    with shortened column names.
+    """
     kobo = KoboExtractor(api_key, end_point)
     results = kobo.get_data(asset_uid=form_id).get('results')
-    return pd.DataFrame(results)
+    df = pd.DataFrame(results)
+
+    # Shorten column names immediately after extraction
+    df.columns = df.columns.str.split('/').str[-1]
+
+    return df
+
 
 def transform_data(df):
-    # Drop duplicates
-    df = df.drop_duplicates(subset=['daycare_name'], keep='first')
-    # Shorten column names
-    df.columns = df.columns.str.split('/').str[-1]
-    # Rename columns
+    """
+    Transform the raw dataframe:
+    - Drop duplicates (based on daycare_name if available)
+    - Rename columns to clean schema
+    """
+    # Drop duplicates safely
+    if 'daycare_name' in df.columns:
+        df = df.drop_duplicates(subset=['daycare_name'], keep='first')
+    else:
+        df = df.drop_duplicates()
+
+    # Rename columns to standardized names
     columns_dict = {
         'kidogo_code': 'Code',
         'county': 'County',
@@ -25,7 +42,11 @@ def transform_data(df):
     }
     return df.rename(columns=columns_dict)
 
+
 def load_to_bigquery(df, table_id):
+    """
+    Load a dataframe into BigQuery, overwriting the table.
+    """
     client = bigquery.Client()
     job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE")
     job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
