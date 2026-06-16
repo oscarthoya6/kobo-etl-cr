0c9fbc9c0e2aa6a1d8c65caa5c660b39d991ced4
oscar <oscarthoya91@gmail.com>
2026-06-16 12:04:41 +0300

    Update ETL logic to use Google Sheets credentials from environment variable

--- etl_logic.py
+++ etl_logic.py
@@ -1,58 +1,22 @@
-import pandas as pd
-from google.cloud import bigquery
-from koboextractor import KoboExtractor
+import os
+import json
 import gspread
 from gspread_dataframe import set_with_dataframe
 from google.oauth2.service_account import Credentials
 
-def extract_data(api_key, end_point, form_id):
-    kobo = KoboExtractor(api_key, end_point)
-    results = kobo.get_data(asset_uid=form_id).get('results')
-    df = pd.DataFrame(results)
-
-    # Shorten column names
-    df.columns = df.columns.str.split('/').str[-1]
-
-    # Drop problematic nested/struct columns
-    drop_cols = ['_attachments', '_geolocation', '_tags', '_notes', '_validation_status']
-    df = df.drop(columns=[c for c in drop_cols if c in df.columns])
-
-    return df
-
-
-def transform_data(df):
-    if 'daycare_name' in df.columns:
-        df = df.drop_duplicates(subset=['daycare_name'], keep='first')
-    else:
-        df = df.drop_duplicates()
-
-    columns_dict = {
-        'kidogo_code': 'Code',
-        'county': 'County',
-        'subcounty': 'SubCounty',
-        'ward': 'Ward',
-        'daycare_name': 'CenterName',
-        'center_type': 'CenterType',
-        'workshop_date': 'WorkshopDate',
-        'MMP_joining_QIP': 'MMPJoiningQIP'
-    }
-    return df.rename(columns=columns_dict)
-
-
-def load_to_bigquery(df, table_id):
-    client = bigquery.Client()
-    job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE")
-    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
-    job.result()
-    print("✅ Data loaded into BigQuery:", table_id)
-
-
-def export_to_google_sheet(df, sheet_name, work_sheet_name, creds_path="service_account.json"):
+def export_to_google_sheet(df, sheet_name, work_sheet_name):
     scopes = [
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive"
     ]
-    creds = Credentials.from_service_account_file(creds_path, scopes=scopes)
+
+    # Read secret JSON from environment variable
+    secret_json = os.environ.get("GOOGLE_SHEETS_CREDENTIALS")
+    if not secret_json:
+        raise RuntimeError("Missing GOOGLE_SHEETS_CREDENTIALS environment variable")
+
+    creds_info = json.loads(secret_json)
+    creds = Credentials.from_service_account_info(creds_info, scopes=scopes)
     gc = gspread.authorize(creds)
 
     try:
